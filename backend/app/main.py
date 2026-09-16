759999469dec3ea27b3169176156386e26622098
edagener0 <153123218+edagener0@users.noreply.github.com>
2026-09-16 12:13:33 +0100

    fix: docker compose

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,10 +7,6 @@ from app.domains.health.router import router as health_router
 def create_app() -> FastAPI:
     app = FastAPI(title=settings.PROJECT_NAME, debug=settings.DEBUG)
 
-    @app.get("/", tags=["root"])
-    def root() -> dict[str, str]:
-        return {"status": "API is running"}
-
     app.include_router(health_router, prefix=settings.API_V1_PREFIX)
 
     return app
