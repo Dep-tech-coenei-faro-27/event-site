dabdc68e2bfd423e285e5303845096c40086add6
Techirocat <guerreirom230@gmail.com>
2026-09-26 20:09:58 +0100

    merge branch feat/fag-accordion and feat/contact-support-form

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,6 +7,7 @@ function Home() {
             <h1 className="text-3xl font-bold">Pagina inicial </h1>
             <p> Bem-vindo ao ENEI!</p>
             <FaqSection />
+
             <ContactSupportForm />
         </div>
     )
